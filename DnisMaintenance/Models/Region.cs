38bd3ad9e51bed8ec09fbdcf215eb34d71a453a3
Jay McCormick <jay.ctiguru@gmail.com>
2023-02-01 10:57:29 -0800

    Add project files.

---
+++ DnisMaintenance/Models/Region.cs
@@ -0,0 +1,8 @@
+﻿namespace DnisMaintenance.Models;
+
+public class Region
+{
+    public string Code { get; set; }
+    public string Description { get; set; }
+}
+
---
+++ DnisMaintenance/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Fast.Components.FluentUI;
+
+namespace DnisMaintenance;
+
+public static class Program
+{
+    public static async Task Main( string[] args )
+    {
+        WebAssemblyHostBuilder? builder = WebAssemblyHostBuilder.CreateDefault( args );
+        builder.RootComponents.Add<App>( "#app" );
+        builder.RootComponents.Add<HeadOutlet>( "head::after" );
+
+        builder.Services
+               .AddScoped( sp => new HttpClient { BaseAddress = new Uri( builder.HostEnvironment.BaseAddress ) } );
+        builder.Services.AddFluentUIComponents();
+
+        builder.Services
+               .AddMsalAuthentication( options =>
+               {
+                   builder.Configuration.Bind( "AzureAd", options.ProviderOptions.Authentication );
+               } );
+
+        await builder.Build().RunAsync().ConfigureAwait( false );
+    }
+}
