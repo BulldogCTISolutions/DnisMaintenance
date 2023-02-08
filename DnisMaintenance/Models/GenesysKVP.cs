4205a7eedb97892dc0dd1088f7c062b62cb43f85
Jay McCormick <jay.ctiguru@gmail.com>
2023-02-08 15:43:17 -0800

    Removed MSAL

---
+++ DnisMaintenance/Models/GenesysKVP.cs
@@ -0,0 +1,23 @@
+﻿namespace DnisMaintenance.Models;
+
+public class GenesysKVP : IComparable
+{
+    public bool Disabled { get; }
+    public bool Selected { get; set; }
+    public string Value { get; set; }
+    public string Text { get; set; }
+
+    public int CompareTo( object? obj )
+    {
+        return obj is not GenesysKVP other
+            ? 1
+            : string.IsNullOrEmpty( this.Text ) && string.IsNullOrEmpty( other.Text )
+               ? 0
+               : string.IsNullOrEmpty( this.Text )
+                 ? -1
+                 : string.IsNullOrEmpty( other.Text )
+                   ? 1
+                   : string.Compare( this.Text, other.Text, StringComparison.OrdinalIgnoreCase );
+    }
+}
+
---
+++ DnisMaintenance/Models/Region.cs
@@ -0,0 +1,21 @@
+﻿namespace DnisMaintenance.Models;
+
+public class Region : IComparable
+{
+    public string Code { get; set; }
+    public string Description { get; set; }
+
+    public int CompareTo( object? obj )
+    {
+        return obj is not Region other
+            ? 1
+            : string.IsNullOrEmpty( this.Description ) && string.IsNullOrEmpty( other.Description )
+               ? 0
+               : string.IsNullOrEmpty( this.Description )
+                 ? -1
+                 : string.IsNullOrEmpty( other.Description )
+                   ? 1
+                   : string.Compare( this.Description, other.Description, StringComparison.OrdinalIgnoreCase );
+    }
+}
+
